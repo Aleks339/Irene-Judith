b64d07ca7e6c88caeda545b1cd2311d66e06e258
alex339 <anna-muljar@rambler.ru>
2017-06-02 18:25:00 +0300

    final changes

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your problem to solve is: <?php echo $_POST["problem"]; ?> <br>
+Your email address is: <?php echo $_POST["email"]; ?> <br>
+
+</body>
+</html>
