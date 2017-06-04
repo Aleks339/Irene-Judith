ca7cdf81eeda0c904a8aa57713609a05b916764d
Vasilii Chugunov <naukomania@gmail.com>
2017-06-04 13:25:17 +0300

    php changed

--- action_page.php
+++ action_page.php
@@ -1,9 +1,9 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your problem to solve is: <?php echo $_POST["problem"]; ?> <br>
-Your email address is: <?php echo $_POST["email"]; ?> <br>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your problem to solve is: <?php echo $_GET["problem"]; ?> <br>
+Your email address is: <?php echo $_GET["email"]; ?> <br>
 
 </body>
 </html>
